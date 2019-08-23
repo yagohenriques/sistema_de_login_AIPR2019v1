5d21cb0536c8786b5e301e72816e5472c7d8b1eb
yago <yagohenriquedossantos28@gmail.com>
2019-08-23 16:45:07 -0300

    Recepção de dados para cadastro de usuário

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
 
+## aula05 - 23/08/2019
+Criação do banco de dados e cadastro de usuário
+
 ## aula04 - 15/08/2019
 Recepção de dados no Back-end
 
--- recebe.php
+++ recebe.php
@@ -1,10 +1,46 @@
 <?php
 
-/**
- * #Back-end
- *  aula04 15/08/2019 - Recepção de dados no Back-end
- */
-echo "<pre>\n";
-print_r($_POST);
-echo "\n</pre>\n";
-print("Alo Ha!");
+//É necessário fazer a conexão com o Banco de Dados
+require_once "configDB.php";
+
+
+function verificar_entrada($entrada)
+{
+    $saida = trim($entrada); //Remova espaços antes e depois
+    $saida = stripslashes($saida); //remove as barras
+    $saida = htmlspecialchars($saida);
+    return $saida;
+}
+
+if(isset($_POST['action']) &&
+    $_POST['action'] == 'cadastro'){
+        $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+        $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+        $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+        $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
+        $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
+        $concordar = $_POST['concordar'];
+        $dataCriacao = date("Y-m-d H:i:s");
+
+        $senha = sha1($senhaUsuario);
+        $senhaC = sha1($senhaConfirma);
+
+        echo "<h5>Nome completo: $nomeCompleto</h5>";
+        echo "<h5>Nome Usuário: $nomeUsuario</h5>";
+        echo "<h5>E-mail Usuário: $emailUsuario</h5>";
+        echo "<h5>Senha Usuário: $senhaUsuario</h5>";
+        echo "<h5>Senha Confirma: $senhaConfirma</h5>";
+        echo "<h5>Concordar: $concordar</h5>";
+        echo "<h5>Data de Criação: $dataCriacao</h5>";
+
+        if($senha != $senhaC){
+            echo "<h1>As senhas não conferem</h1>";
+            exit();
+
+        }else{
+            echo "<h5> senha codificada: $senha</h5>";
+        }
+    }else{
+        echo "<h1 'style = color: red'> Está página não pode ser acessado diretamente</h1>";
+    }
+    
