26a773264a09c5bc38cbcd98b82809c088c6db7d
yago <yagohenriquedossantos28@gmail.com>
2019-08-23 15:08:43 -0300

    Conexão com o banco de dados

---
+++ configDB.php
@@ -0,0 +1,16 @@
+<?php
+//configDB.php
+
+//Dados para escolha do DataBase (DB)
+$dbhost = "localhost";
+$dbuser = "root"; //Usuário Raíz (Rute)
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+//Conexão com o banco de dados
+$conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($conecta->connect_error){
+    die("Não foi possível conectar ao Banco de Dados: " . $conecta->connect_error);
+}else{
+    echo "<h1>Conectou no BD Manowwwww!</h1>";
+}
