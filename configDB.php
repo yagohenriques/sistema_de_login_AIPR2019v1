f35d78c02b89b498d6fb61c1e06730f624ed80d8
yagohenriques <49690675+yagohenriques@users.noreply.github.com>
2019-08-23 15:40:30 -0300

    Update configDB.php

--- configDB.php
+++ configDB.php
@@ -12,5 +12,5 @@ $conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if($conecta->connect_error){
     die("Não foi possível conectar ao Banco de Dados: " . $conecta->connect_error);
 }else{
-    echo "<h1>Conectou no BD Manowwwww!</h1>";
+    //echo "<h1>Conectou no BD Manowwwww!</h1>";
 }
